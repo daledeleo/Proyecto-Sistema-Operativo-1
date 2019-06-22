4957684b508bf346dbf4e541e800e150d1f53c14
daledeleo <dalede@espol.edu.ec>
2019-06-22 15:54:47 -0500

    comienzo del proyecto

---
+++ conurrente_filtro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <pthread.h>
+#include "errors.h"
+
+using namespace cv;
+
+void *thread_fun(void *arg){
+
+}
+// Declaracion de las variables a usarse 
+Mat src, dst, dstx, dsty;
+Mat kernel;
+Point anchor;
+double delta;
+int ddepth;
+int kernel_size;
+
+int main (int argc, char** argv){
+    
+    return 1;
+}
