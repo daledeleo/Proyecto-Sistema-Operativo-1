aea9d842d9ff122958b2bdc0346180130570025d
daledeleo <dalede@espol.edu.ec>
2019-06-29 09:44:15 -0500

    ddd

--- concurrente.cpp
+++ concurrente.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -36,42 +37,40 @@ Mat imgs[NUM_HILOS];
     |    |    | 
     |____|____|
     */
-void *f_top_leftandprocess(void *rt){
-    top_left = img(cv::Range(0, img.rows / 2 - 1), cv::Range(0, img.cols / 2 - 1));
-    filter2D(top_left, top_left, ddepth , kernel,anchor, delta,BORDER_DEFAULT);
+void *thread_process(void *rt){
+    if(strcmp("top_left",(char *)rt)==0){
+        anchor = Point( -1, -1 );
+        top_left = img(cv::Range(0, img.rows / 2 - 1), cv::Range(0, img.cols / 2 - 1));
+        filter2D(top_left, top_left, ddepth , kernel,anchor, delta,BORDER_DEFAULT);
+    }else if(strcmp("top_right",(char *)rt)==0){
+        anchor = Point( -1, -1 );
+        top_right= img(cv::Range(0, img.rows / 2 - 1), cv::Range(img.cols / 2, img.cols - 1));
+        filter2D(top_right, top_right, ddepth , kernel,
+        anchor, delta,BORDER_DEFAULT );
+    }else if(strcmp("bottom_left",(char *)rt)==0){
+        anchor = Point( -1, -1 );
+        bottom_left = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(0, img.cols / 2 - 1));
+        filter2D(bottom_left, bottom_left, ddepth , kernel,
+        anchor, delta,BORDER_DEFAULT );
+    }else{
+        anchor = Point( -1, -1 );
+        bottom_right = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(img.cols / 2, img.cols - 1));
+        filter2D(bottom_right, bottom_right, ddepth , kernel,
+        anchor, delta,BORDER_DEFAULT );
+    }
     pthread_exit(0);
 }
 
-void *f_top_rightandProcess(void *rt){
-    top_right= img(cv::Range(0, img.rows / 2 - 1), cv::Range(img.cols / 2, img.cols - 1));
-    filter2D(top_right, top_right, ddepth , kernel,
-    anchor, delta,BORDER_DEFAULT );
-    pthread_exit(0);
-}
-
-void *f_bottom_leftandProcess(void *rt){
-    bottom_left = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(0, img.cols / 2 - 1));
-    filter2D(bottom_left, bottom_left, ddepth , kernel,
-    anchor, delta,BORDER_DEFAULT );
-    pthread_exit(0);
-}
-
-void *f_bottom_rightandProcess(void *rt){
-    bottom_right = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(img.cols / 2, img.cols - 1));
-    filter2D(bottom_right, bottom_right, ddepth , kernel,
-    anchor, delta,BORDER_DEFAULT );
-    pthread_exit(0);
-}
 void *mostrar_img(void *hg){
-    imshow("imagen principal",img2);
+    imshow("imagen principal",img);
     pthread_testcancel();
 }
 int main (int argc, char** argv){
     pid_t pid;
     int status;
-    img2=imread(argv[1]);
     //cargando la imagen
     img=imread(argv[1]);
+    imshow("imagen principal",img);
     
     //Si no se puede cargar la imagen el programa se detiene
     if(!img.data){
@@ -89,44 +88,41 @@ int main (int argc, char** argv){
     }else if(pid==0){ //Proceso hijo 
         
         //se crea 2 hilosde trabajo
-        pthread_create(&thread_id1,NULL,f_top_leftandprocess,NULL);
-        pthread_create(&thread_id2,NULL,f_top_rightandProcess,NULL);
+        pthread_create(&thread_id1,NULL,thread_process,(void *)"top_left");
+        pthread_create(&thread_id2,NULL,thread_process,(void *)"top_right");
         pthread_join(thread_id1,NULL);
         pthread_join(thread_id2,NULL);
         imwrite("output1.jpg",top_left); 
         imwrite("output2.jpg",top_right); 
         
     }else{
-        pthread_create(&thread_id,NULL,mostrar_img,NULL);
-        pthread_create(&thread_id3,NULL,f_bottom_leftandProcess,NULL);
-        pthread_create(&thread_id4,NULL,f_bottom_rightandProcess,NULL);
+        pthread_create(&thread_id3,NULL,thread_process,(void*)"bottom_left");
+        pthread_create(&thread_id4,NULL,thread_process,(void *)"bottom_right");
         waitpid(pid, &status, WUNTRACED | WCONTINUED);
         top_left=imread("./output1.jpg");
         top_right=imread("./output2.jpg");
+        
+        hconcat(bottom_left,bottom_right,bottom_left);
         pthread_join(thread_id3,NULL);
         pthread_join(thread_id4,NULL);
-        pid_t y=fork();
-        if(y==0){
-            hconcat(bottom_left,bottom_right,bottom_left);
-            hconcat(top_left,top_right,top_left);
-            vconcat(top_left,bottom_left,bottom_left);
-            imshow("imagen final",bottom_left);
-        }else{
-            pthread_join(thread_id,NULL);
-            waitpid(y, &status, WUNTRACED | WCONTINUED);
-            waitKey(0);
-        }
-        exit(0);
+        hconcat(top_left,top_right,top_left);
+        vconcat(top_left,bottom_left,bottom_left);            
+        imshow("imagen final1",bottom_left);
+        //imshow("imagen final2",bottom_right);
+        //imshow("imagen final3",top_left);
+        //imshow("imagen final4",top_right);
+
+        waitKey(0);
         //execv("/bin/rm ./output1.jpg ./output2.jpg",NULL);
     }
-        exit(0);
+        return 1;
 
 }
 
 void inicializar(){
-    anchor = Point( -1, -1 );
+    
     delta = 0;
     ddepth = -1;
-    //Gaussian
+    //Gaussian 15
     kernel=getGaussianKernel(15,3);
 }
