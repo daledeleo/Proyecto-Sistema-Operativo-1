9a3057b1467984856c81bfcb7d253e611f620524
daledeleo <dalede@espol.edu.ec>
2019-06-29 08:23:25 -0500

    varios cambios

--- concurrente.cpp
+++ concurrente.cpp
@@ -3,64 +3,130 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /* numeros de hilos y numeros de partes en la que se
-va a dividir la imagen*/
+va a dividir la imagen solo puede ser 4*/
 #define NUM_HILOS    4   
 using namespace cv;
+void inicializar();
+void *f_top_leftandprocess(void *rt);
+void *f_top_rightandprocess(void *rt);
+void *f_bottom_leftandProcess(void *rt);
+void *f_bottom_rightandprocess(void *rt);
 
-Mat src, dst;
+//Variables globales
+Mat img, dst;
+Mat img2;
 Mat kernel;
 Point anchor;
 double delta;
 int ddepth;
-pthread_t thread_id;
-pthread_t threads_id[NUM_HILOS];
+pthread_t thread_id,thread_id1,thread_id2,thread_id3,thread_id4;
+Mat top_left,top_right,bottom_left,bottom_right;
+Mat imgs[NUM_HILOS];
 
-void *thread_funPoint(void *arg){
+/*  Se procede a dividir la imagen en partes iguales para que cada hilo 
+    trate a cada parte de la imagen
+    __________   
+    |    |    |
+    |____|____|
+    |    |    | 
+    |____|____|
+    */
+void *f_top_leftandprocess(void *rt){
+    top_left = img(cv::Range(0, img.rows / 2 - 1), cv::Range(0, img.cols / 2 - 1));
+    filter2D(top_left, top_left, ddepth , kernel,anchor, delta,BORDER_DEFAULT);
+    pthread_exit(0);
+}
+
+void *f_top_rightandProcess(void *rt){
+    top_right= img(cv::Range(0, img.rows / 2 - 1), cv::Range(img.cols / 2, img.cols - 1));
+    filter2D(top_right, top_right, ddepth , kernel,
+    anchor, delta,BORDER_DEFAULT );
+    pthread_exit(0);
+}
+
+void *f_bottom_leftandProcess(void *rt){
+    bottom_left = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(0, img.cols / 2 - 1));
+    filter2D(bottom_left, bottom_left, ddepth , kernel,
+    anchor, delta,BORDER_DEFAULT );
+    pthread_exit(0);
+}
+
+void *f_bottom_rightandProcess(void *rt){
+    bottom_right = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(img.cols / 2, img.cols - 1));
+    filter2D(bottom_right, bottom_right, ddepth , kernel,
+    anchor, delta,BORDER_DEFAULT );
+    pthread_exit(0);
+}
+void *mostrar_img(void *hg){
+    imshow("imagen principal",img2);
+    pthread_testcancel();
+}
+int main (int argc, char** argv){
+    pid_t pid;
+    int status;
+    img2=imread(argv[1]);
+    //cargando la imagen
+    img=imread(argv[1]);
+    
+    //Si no se puede cargar la imagen el programa se detiene
+    if(!img.data){
+        return -1;
+    }
+    
+
     // Initialize arguments for the filter
-    Mat dst;
+    inicializar();
+
+    pid=fork();
+    if(pid<0){
+        perror("fork");
+        exit(1);
+    }else if(pid==0){ //Proceso hijo 
+        
+        //se crea 2 hilosde trabajo
+        pthread_create(&thread_id1,NULL,f_top_leftandprocess,NULL);
+        pthread_create(&thread_id2,NULL,f_top_rightandProcess,NULL);
+        pthread_join(thread_id1,NULL);
+        pthread_join(thread_id2,NULL);
+        imwrite("output1.jpg",top_left); 
+        imwrite("output2.jpg",top_right); 
+        
+    }else{
+        pthread_create(&thread_id,NULL,mostrar_img,NULL);
+        pthread_create(&thread_id3,NULL,f_bottom_leftandProcess,NULL);
+        pthread_create(&thread_id4,NULL,f_bottom_rightandProcess,NULL);
+        waitpid(pid, &status, WUNTRACED | WCONTINUED);
+        top_left=imread("./output1.jpg");
+        top_right=imread("./output2.jpg");
+        pthread_join(thread_id3,NULL);
+        pthread_join(thread_id4,NULL);
+        pid_t y=fork();
+        if(y==0){
+            hconcat(bottom_left,bottom_right,bottom_left);
+            hconcat(top_left,top_right,top_left);
+            vconcat(top_left,bottom_left,bottom_left);
+            imshow("imagen final",bottom_left);
+        }else{
+            pthread_join(thread_id,NULL);
+            waitpid(y, &status, WUNTRACED | WCONTINUED);
+            waitKey(0);
+        }
+        exit(0);
+        //execv("/bin/rm ./output1.jpg ./output2.jpg",NULL);
+    }
+        exit(0);
+
+}
+
+void inicializar(){
     anchor = Point( -1, -1 );
     delta = 0;
     ddepth = -1;
     //Gaussian
     kernel=getGaussianKernel(15,3);
-    filter2D((src, dst, ddepth , kernel,
-    anchor, delta,BORDER_DEFAULT );
-    return dst;
-}
-/*  Se procede a dividir la imagen en partes iguales para que cada hilo 
-    trate a cada parte de la imagen
-    ____________________
-    |    |    |    |    |
-    |    |    |    |    |
-    |    |    |    |    |
-    |    |    |    |    | 
-    |____|____|____|____|
-    */
-void f_dividirImg(Mat img,Mat imgs[]){
-    Mat img_cut;
-    for(int i=0;i<NUM_HILOS;i++){
-        Rect myROI();
-        // Procedimiento para dividir la imagen
-        
-        imgs[i]=img_cut;
-    }
-}
-
-int main (int argc, char** argv){
-    
-    //cargando la imagen
-    src=imread(argv);
-
-    //Si no se puede cargar la imagen el programa se detiene
-    if(!src.empty()){
-        return -1;
-    }
-    
-    Mat imgs[SIZE];
-    f_dividirImg(src,imgs);
-
-    waitKey(0);
-    return 1;
 }
--- pruebas.cpp
+++ pruebas.cpp
@@ -5,13 +5,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 //g++ threads.c -pthread
 //g++ -o ejecutable holaMundo.cpp
 //sudo g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 filter_demo_SO.cpp -o ejemplo_filtro -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs
 
 using namespace cv;
-pid_t pid;
+
 cv::Mat img;
+cv::Mat top_left,top_right,bottom_left,bottom_right;
 //Mat img;
 /*
 void left(void *g){
@@ -20,25 +23,50 @@ void left(void *g){
 }*/
 int main(int agr,char **argv){
     //pthread_t thread_id1;
+    int status;
+    int fd[2];
+    pipe(fd);
     img= imread(argv[1]);
-    
-    pid=fork();
-    if(pid==0){
-        cv::Mat top_left = img(cv::Range(0, img.rows / 2 - 1), cv::Range(0, img.cols / 2 - 1));
-        cv::Mat top_right= img(cv::Range(0, img.rows / 2 - 1), cv::Range(img.cols / 2, img.cols - 1));
-    }else{
-        cv::Mat bottom_left = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(0, img.cols / 2 - 1));
-        cv::Mat bottom_right = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(img.cols / 2, img.cols - 1));
-    }
-    cv::imshow("top_left", top_left);
-    cv::imshow("top_right", top_right);
-    cv::imshow("bottom_left", bottom_left);
-    cv::imshow("bottom_right", bottom_right);
+    pid_t pid;
 
-    cv::hconcat(bottom_left,bottom_right,bottom_left);
-    cv::hconcat(top_left,top_right,top_left);
-    cv::vconcat(top_left,bottom_left,bottom_left);
-    cv::imshow("imagen final",bottom_left);
-    cv::waitKey(0);
+    pid=fork();
+    
+    if(pid==0){
+        //close(fd[0]);
+        top_left = img(cv::Range(0, img.rows / 2 - 1), cv::Range(0, img.cols / 2 - 1));
+        top_right= img(cv::Range(0, img.rows / 2 - 1), cv::Range(img.cols / 2, img.cols - 1));
+        imwrite("output1.jpg",top_left); 
+        imwrite("output2.jpg",top_right); 
+        //close(fd[1]);
+        printf("pid hijo: %ld\n",(long)getpid());
+        
+        exit(0);
+    }else{
+        top_left=imread("./output1.jpg");
+        top_right=imread("./output2.jpg");
+        bottom_left = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(0, img.cols / 2 - 1));
+        bottom_right = img(cv::Range(img.rows / 2, img.rows - 1), cv::Range(img.cols / 2, img.cols - 1));
+        printf("pid padre: %ld espera por el hijo %ld\n",
+        (long)getpid(),(long)pid);
+        waitpid(pid,&status,WUNTRACED|WCONTINUED);
+        //close(fd[1]);
+        cv::imshow("top_left", top_left);
+        cv::imshow("top_right", top_right);
+        //close(fd[0]);
+        cv::imshow("bottom_left", bottom_left);
+        cv::imshow("bottom_right", bottom_right);
+
+        cv::hconcat(bottom_left,bottom_right,bottom_left);
+        cv::hconcat(top_left,top_right,top_left);
+        cv::vconcat(top_left,bottom_left,bottom_left);
+        cv::imshow("imagen final",bottom_left);
+        
+        argv[0]=(char *)"/bin/rm ./output2.jpg ./output1.jpg";
+        printf(argv[0]);
+        cv::waitKey(0);
+        execvp(argv[0],argv);
+        
+        
+    }
     return 1;
 }
